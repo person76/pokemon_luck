dc8101bf0f289e0e2c9e9c56a947e590f0bbda7b
kookong2 <hy010309@naver.com>
2024-08-18 17:29:21 +0900

    [FEAT] 게임 오버 조건 추가
    ShopService 코드 추가

--- pokemon_luck/src/main/java/com/ohgiraffers/service/ShopService.java
+++ pokemon_luck/src/main/java/com/ohgiraffers/service/ShopService.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.service;
 import com.ohgiraffers.aggregate.Player;
 import com.ohgiraffers.aggregate.BallType;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -44,5 +45,13 @@ public class ShopService {
                 System.out.println(quantity + "개의 " + selectedBall.getName() + "을(를) 구매했습니다.");
             }
         }
+    }
+
+    public int getCheapestBallPrice() {
+        // BallType 배열을 스트림으로 변환하여 가장 저렴한 가격의 볼을 찾음
+        return Arrays.stream(BallType.values())
+                .mapToInt(BallType::getPrice)
+                .min()
+                .orElse(0); // 최소 가격 반환
     }
 }
